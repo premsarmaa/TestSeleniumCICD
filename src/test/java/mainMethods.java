3ca734e4a8a1664e1a9186643565bb1657cc9822
premsarmaa <prem.sarmaa@gmail.com>
2025-11-22 02:13:54 +0530

    added to check github actions

--- src/test/java/mainMethods.java
+++ src/test/java/mainMethods.java
@@ -1,15 +1,12 @@
 import java.io.File;
 import java.io.IOException;
-import java.time.Duration;
 
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class mainMethods extends openURL {   
 
